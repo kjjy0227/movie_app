0d183015a1fe3162d2afea711a1c15dc82ecf5e0
jiyoungkim <kjjy0227@gmail.com>
2021-01-03 22:16:00 +0900

    #1.1 Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food(props){
+  console.log(props)
+  return <h1>I like Potato</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!!</h1> 
+      <Food 
+        fav="kimchi" 
+        something={true}
+        papapapa={["hello",1,2,3,4,true]}
+      />
     </div>
   );
 }
 
 export default App;
+ 
